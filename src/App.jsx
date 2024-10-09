82663d1ab4134c6c399be666dcbc4c702e94f6dd
levin <levinthiel@proton.me>
2024-10-09 16:21:22 +0200

    resolved issue 3 - deleting colors from the array

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,24 @@ import { uid } from 'uid';
 function App() {
 
   const [newColors, setnewColors]= useState(initialColors)
+  const [emptyArray, setEmptyArray] = useState(false)
 
   function handleAddColor(event){
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    setnewColors([{id: uid(), ...data}, ...initialColors])
+    setnewColors([{id: uid(), ...data}, ...newColors])
     console.log(data)
     console.log(newColors)
     event.target.reset();
   }
+  function yesDelete(id){
+    const deletedColor = newColors.filter(color => color.id !== id);
+    setnewColors(deletedColor)
+    const message = newColors.length === 1 ? setEmptyArray(true) : "The array is not empty.";
+    console.log(message);
+  }
+
 
 
   return (
@@ -26,8 +34,11 @@ function App() {
 
       < ColorForm onSubmit={handleAddColor} />
       {newColors.map((color) => {
-        return <Color key={color.id} color={color} />;
+        return <Color key={color.id} color={color} onDelete={yesDelete} />;
       })}
+      {emptyArray &&(
+        <p>You have no more colours left, please consider adding some</p>
+      )}
     </>
   );
 }
